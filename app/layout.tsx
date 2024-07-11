33b736026b3777b8971d7d86bab2a8085c076a51
zeff <jzeffsomera@gmail.com>
2024-07-11 15:50:00 +0800

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import { Lato } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 const lato = Lato({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TapUp",
-  description: "Transform your business interactions with TapUp Cards. Our app allows you to create, share, and manage digital business cards effortlessly. With customizable designs, instant updates, and seamless integration across devices, you can leave a lasting impression. Elevate your professional networking to the next level with TapUp Cards.",  
+  description:
+    "Transform your business interactions with TapUp Cards. Our app allows you to create, share, and manage digital business cards effortlessly. With customizable designs, instant updates, and seamless integration across devices, you can leave a lasting impression. Elevate your professional networking to the next level with TapUp Cards.",
 };
 
 export default function RootLayout({
@@ -18,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={lato.className}>
         <main>{children}</main>
         <ToastContainer />
--- src/lib/firebase/config/firebase.ts
+++ src/lib/firebase/config/firebase.ts
@@ -10,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-console.log("firebaseConfigzz:", firebaseConfig);
 
 const firebaseApp = initializeApp(firebaseConfig);
 
