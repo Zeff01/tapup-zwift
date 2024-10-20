01ee438cf834881d093e7626b81c594d85c2fa37
Joseph Jamolod <josephjamolod@gmail.com>
2024-10-20 19:25:48 +0800

    refactor: remove some properties in each button

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -2,24 +2,14 @@ import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { Button } from "../ui/button";
 
-export const Social = ({ method }: { method: string }) => {
+export const Social = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-x-2 gap-y-[20px]">
-      <Button
-        className="w-full border-black rounded-full h-[46px] gap-x-2"
-        size={"lg"}
-        variant={"outline"}
-      >
+    <div className="flex items-center justify-center w-full gap-x-2">
+      <Button className="w-full border-black" size={"lg"} variant={"outline"}>
         <FcGoogle className="text-2xl" />
-        <span className="text-muted-foreground">{method} with Google</span>
       </Button>
-      <Button
-        className="w-full border-black rounded-full h-[46px] gap-x-2"
-        size={"lg"}
-        variant={"outline"}
-      >
-        <FaFacebook className="text-2xl text-blue-500" />
-        <span className="text-muted-foreground">{method} with Google</span>
+      <Button className="w-full border-black" size={"lg"} variant={"outline"}>
+        <FaFacebook className="text-2xl text-blue-600" />
       </Button>
     </div>
   );
