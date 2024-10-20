e62546741f8d7c45f1bad5cbf13ea04d6d552133
Joseph Jamolod <josephjamolod@gmail.com>
2024-10-20 19:58:42 +0800

    refactor: modify padding top

--- components/auth/RememberButton.tsx
+++ components/auth/RememberButton.tsx
@@ -1,6 +1,6 @@
 export const RememberButton = () => {
   return (
-    <div className="py-[12px] pb-[20px] flex justify-between items-center ">
+    <div className="pt-2 flex justify-between items-center ">
       <div className="flex gap-x-2">
         <input type="checkbox" />
         <span className="font-mono text-sm">Remember password</span>
