a15a6d38dfd91ca0b34f74e828d33c0439217c7c
Joseph Jamolod <josephjamolod@gmail.com>
2024-10-20 14:13:50 +0800

    feat: complete navigator.tsx component

---
+++ app/auth/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Navigator from "@/components/auth/Navigator";
+import { useState } from "react";
+
+export default function AuthPage() {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <main className="w-full  min-h-screen flex items-center justify-center  px-5">
+      <div className="w-[1200px]  min-h-[700px] flex md:items-center relative">
+        <div className="w-full  md:h-[400px]  flex flex-col md:flex-row bg-gradient-to-bl from-[#21C15C]  bg-[#419a50] rounded-lg">
+          <Navigator
+            start={true}
+            setToggle={setToggle}
+            buttonLabel={"Register Now"}
+            msg="Don't have an account?"
+          />
+          <Navigator
+            start={false}
+            setToggle={setToggle}
+            buttonLabel={"Sign In"}
+            msg="Already have an account?"
+          />
+        </div>
+        <div
+          className={`absolute bg-white w-full md:w-1/2 h-3/4 md:h-[650px] text-black flex items-center justify-center overflow-hidden transfrom ${
+            toggle
+              ? "translate-y-0 md:translate-x-0"
+              : "translate-y-[33%] md:translate-y-0 md:translate-x-full"
+          }  transition-transform duration-500 ease-in-out`}
+        >
+          <div
+            style={{
+              transform: toggle ? "translateX(0)" : "translateX(-100%)",
+            }}
+            className={`bg-gray-400  w-full absolute transition-transform duration-500 ease-in-out`}
+          >
+            <button>Content 1</button>
+          </div>
+          <div
+            style={{
+              transform: toggle ? "translateX(100%)" : "translateX(0)",
+            }}
+            className={`bg-pink-300 w-full  transition-transform duration-500 ease-in-out`}
+          >
+            <button>Content 2</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
