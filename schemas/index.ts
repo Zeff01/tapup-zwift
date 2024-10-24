2c06fc35aa85d21f177f0f2ecefb9996729f3da0
Joseph Jamolod <josephjamolod@gmail.com>
2024-10-24 11:55:59 +0800

    refactor: modify log-in schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().min(1, { message: "Please enter your" }).email(),
+  email: z.string().min(1, { message: "Please enter your email" }).email(),
   password: z.string().min(1, { message: "Please enter your password" }),
 });
 
