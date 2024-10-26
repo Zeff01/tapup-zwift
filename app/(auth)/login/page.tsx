d32de2238b66285e4f5529db58f18af9a4c5b760
Joseph Jamolod <josephjamolod@gmail.com>
2024-10-26 09:59:25 +0800

    feat: create sign-in-page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,15 @@
 import BackGround from "../components/back-ground";
-import { RegisterForm } from "../components/RegisterForm";
+import { LogInForm } from "../components/LoginForm";
 
-export default function SignUpPage() {
+export default function LogInPage() {
   return (
     <main className="flex justify-center items-center  min-h-screen p-4">
-      <div className="flex bg-[#21C15C] max-w-[900px] w-full  rounded-md">
-        <div className="hidden md:flex flex-1 items-center justify-center overflow-hidden ">
+      <div className="flex bg-[#21C15C] h-[577px] max-w-[900px] w-full  rounded-md">
+        <div className="hidden md:flex flex-1 items-center rounded-md overflow-hidden">
           <BackGround />
         </div>
         <div className="flex flex-1 items-center justify-center z-10">
-          <RegisterForm />
+          <LogInForm />
         </div>
       </div>
     </main>
