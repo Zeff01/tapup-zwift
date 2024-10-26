0aee3652e09fbbd225cf4325df8a7a1e40bb10f2
reyleal27 <reyvincentleal@gmail.com>
2024-10-26 23:04:52 +0800

    perf: optimize image

--- components/landing/about-tapup.tsx
+++ components/landing/about-tapup.tsx
@@ -5,12 +5,12 @@ import demoCard from "@/public/assets/tap-up-demo-card.png";
 
 const AboutTapup = () => {
   return (
-    <section className="flex flex-col lg:flex-row lg:justify-center items-center py-[79px] shadow-xl">
-      <div className="w-[50%] lg:relative right-[15%] top-[-10rem]">
-        <Image src={cardImg} alt="Tap up card Image" loading="lazy" />
+    <section className="flex flex-col lg:flex-row items-center justify-center lg:py-[79px] py-[3rem] shadow-xl">
+      <div className="xl:w-[50%] lg:h-[70vh]  md:h-[50vh] h-[20vh] w-[60%] relative lg:right-[15%]">
+        <Image src={cardImg} alt="Tap up card Image" fill className="object-cover"/>
       </div>
-      <div className="flex flex-col gap-3 w-[40%]">
-        <h2 className="text-4xl lg:text-[3rem] font-semibold italic font-sans">
+      <div className="flex flex-col gap-3 lg:w-[40%] w-[60vw]">
+        <h2 className="text-4xl lg:text-[3rem] font-semibold italic font-sans pb-3">
           Tap<span className="text-green-600"> Up</span>
         </h2>
         <p className="text-xs lg:text-base text-[#524E4E] leading-[28px]">
@@ -29,7 +29,9 @@ const AboutTapup = () => {
         <Button className="bg-green-500 hover:bg-green-700 w-[11rem] font-bold uppercase ">
           Learn More
         </Button>
-        <Image src={demoCard} alt="Demo Card Image" loading="lazy" />
+        <div className="relative w-[60vw] aspect-video lg:w-[30vw] h-[14rem]">
+        <Image src={demoCard} alt="Demo Card Image" fill className="object-contain" />
+        </div>
       </div>
     </section>
   );
