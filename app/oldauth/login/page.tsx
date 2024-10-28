718960d98ea174ac8397eada754163cb6a977a7d
lonzo <jmmaglonzo.dev@gmail.com>
2024-10-28 21:07:35 +0800

    chore: delete old components and unused codes

--- app/oldauth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import OldLoginForm from "../_components/login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex items-center justify-center bg-[#1E1E1E] min-h-screen w-full">
-      <OldLoginForm />
-    </div>
-  );
-}
