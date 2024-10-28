c1b2047941e76028cbcf59dd2792450c2ba45202
reyleal27 <reyvincentleal@gmail.com>
2024-10-28 18:22:41 +0800

    style: change image style(format)

--- components/landing/shopping-card.tsx
+++ components/landing/shopping-card.tsx
@@ -1,9 +1,4 @@
-import Image, { StaticImageData } from "next/image";
-import tapUpCard from "@/public/assets/tapUp-card1.png";
-import tapUpCard2 from "@/public/assets/tapUp-card2.png";
-import tapUpCard3 from "@/public/assets/tapUp-card3.png";
-import tapUpCard4 from "@/public/assets/tapUp-card4.png";
-import tapUpCard5 from "@/public/assets/tapUp-card5.png";
+import Image from "next/image";
 import {
   Card,
   CardContent,
@@ -16,7 +11,7 @@ import { Button } from "../ui/button";
 import ExpandableText from "../ExpandableText";
 
 interface CardItem {
-  image: StaticImageData;
+  image: string;
   title: string;
   description: string;
   price: number;
@@ -24,42 +19,42 @@ interface CardItem {
 
 const cardItems: CardItem[] = [
   {
-    image: tapUpCard,
+    image: "/assets/tapUp-card1.png",
     title: "Standard Black Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj ",
     price: 3000,
   },
   {
-    image: tapUpCard2,
+    image: "/assets/tapUp-card2.png",
     title: "Standard Yellow Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj sed do eiusmod tempor incididunt elweofsaj sed do eiusmod tempor incididunt elweofsaj ",
     price: 3000,
   },
   {
-    image: tapUpCard3,
+    image: "/assets/tapUp-card3.png",
     title: "Standard Light Blue Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj ",
     price: 4000,
   },
   {
-    image: tapUpCard4,
+    image: "/assets/tapUp-card4.png",
     title: "Standard Blue Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj ",
     price: 2500,
   },
   {
-    image: tapUpCard5,
+    image: "/assets/tapUp-card5.png",
     title: "Standard White Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj ",
     price: 3600,
   },
   {
-    image: tapUpCard2,
+    image: "/assets/tapUp-card2.png",
     title: "Standard Yellow Card",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elweofsaj ",
@@ -77,33 +72,31 @@ const ShoppingCard = () => {
       <h2 className="text-center italic font-medium font-inter text-[2.5rem]">
         Shopping Card
       </h2>
-      <div className="mt-[6rem] flex flex-wrap justify-center gap-7">
+      <div className="lg:mt-[6rem] mt-10 flex flex-wrap relative justify-center gap-7">
         {cardItems.map((item, index) => (
           <Card
             key={index}
-            className="border rounded-lg shadow-md lg:w-[384px] w-[250px] justify-center"
+            className="border rounded-lg md:w-[24rem] w-[18rem] shadow-md justify-center"
           >
-            <CardHeader className="p-0">
+            <CardHeader className="relative w-full aspect-video p-0 mt-2">
               <Image
                 src={item.image}
                 alt={item.title}
-                width={300}
-                height={300}
-                loading="lazy"
-                className="w-[70rem] h-[15rem] p-0"
+                fill
+                className="object-cover"
               />
-              <CardTitle className="text-lg font-bold mt-2 ml-4">
+            </CardHeader>
+            <CardContent className="p-0">
+              <CardTitle className="text-lg font-bold mt-2 px-5">
                 {item.title}
               </CardTitle>
-            </CardHeader>
-            <CardContent className="px-4">
-              <CardDescription className="text-xs">
+              <CardDescription className="text-xs px-4">
                 <ExpandableText descriptionLength={100}>
                   {item.description}
                 </ExpandableText>
               </CardDescription>
             </CardContent>
-            <CardFooter className="flex justify-between pt-5">
+            <CardFooter className="flex justify-between pt-5 px-4">
               <span className="lg:text-3xl text-xl font-semibold">
                 &#x20B1;{formatPrice(item.price)}
               </span>
