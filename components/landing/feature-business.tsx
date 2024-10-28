3fdc5bb11019dcd774040bbc24f0db254fb04976
reyleal27 <reyvincentleal@gmail.com>
2024-10-28 18:24:13 +0800

    style: edit image format

--- components/landing/feature-business.tsx
+++ components/landing/feature-business.tsx
@@ -3,11 +3,24 @@ import pay from "@/public/assets/pay-gray.png";
 import paypal from "@/public/assets/paypal-gray.png";
 import Image from "next/image";
 
-const businessList = [amazon, pay, paypal, amazon, pay, paypal];
+const businessList = [
+  {
+    title: "Amazon",
+    image: "/assets/amazon-gray.png",
+  },
+  {
+    title: "Pay",
+    image: "/assets/pay-gray.png",
+  },
+  {
+    title: "Paypal",
+    image: "/assets/paypal-gray.png",
+  },
+];
 
 const FeaturedBusiness = () => {
   return (
-    <section className="bg-[#1FAE3A33] py-11">
+    <section className="bg-[#1FAE3A33] py-8">
       <div className="h-[155px]">
         <h4 className="text-center uppercase font-inter text-2xl tracking-widest">
           trusted by 30+ businesses
@@ -15,12 +28,14 @@ const FeaturedBusiness = () => {
 
         <div className="flex justify-evenly items-center">
           {businessList.map((item, index) => (
-            <Image
-              src={item}
-              key={index}
-              alt={`image ${index}`}
-              loading="lazy"
-            />
+            <div key={index} className="relative md:w-[12rem] md:h-[8rem] h-[6rem] w-[8rem]">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-contain"
+              />
+            </div>
           ))}
         </div>
       </div>
