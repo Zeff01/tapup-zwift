f0f2931bf0cdad9d0f74cca4abf4df14bc03babb
reyleal27 <reyvincentleal@gmail.com>
2024-10-28 11:54:50 +0800

    feat: make title dynamic

--- components/landing/card-details.tsx
+++ components/landing/card-details.tsx
@@ -1,13 +1,17 @@
 import { Button } from "../ui/button";
 import { HiArrowRightEndOnRectangle } from "react-icons/hi2";
 
-const CardDetails = () => {
+interface CardDetailsProps {
+    title: string;
+}
+
+const CardDetails: React.FC<CardDetailsProps> = ({ title }) => {
   return (
-    <section className="mt-[4rem]  mx-auto w-[80vw]">
+    <section className="mt-[2rem]  mx-auto w-[80vw]">
       <h2 className="text-center text-3xl font-inter uppercase text-[#3B3636]">
-        Standard Black Card
+        {title}
       </h2>
-      <p className="pt-[3rem] text-2xl text-[#524E4E] items-center">
+      <p className="pt-[3rem] text-xl text-justify md:text-2xl text-[#524E4E] items-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         consectetur adipiscing elit, sed do tempor incididunt ut labore et
         dolore magna aliqua. Ut enim ad minim incididunt ut labore et dolore
@@ -16,7 +20,7 @@ const CardDetails = () => {
         ad minim incididunt ut labore et dolore magna consectetur adipiscing
         elit
       </p>
-      <p className="pt-[3rem] text-2xl text-[#524E4E] items-center">
+      <p className="pt-[3rem] text-xl text-justify md:text-2xl text-[#524E4E] items-center">
         ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         consectetur adipiscing elit, sed do sed adipiscing elit, sed do eiusmod
         consectetur adipiscing elit, sed do dolore magna aliqua. Ut enim ad
