5e897efb9796f27f4607b655529acc3f3487a6ff
reagan13 <reaganmabawad@gmail.com>
2024-10-31 17:41:44 +0800

    feat: add constants for password reset and forgot password routes

--- middleware.ts
+++ middleware.ts
@@ -1,53 +1,53 @@
 import { type NextRequest, NextResponse } from "next/server";
 import {
-  SESSION_COOKIE_NAME,
-  LOGIN_ROUTE,
-  SIGNUP_ROUTE,
-  ONBOARDING_ROUTE,
-  CARD_ROUTE,
-  DASHBOARD_ROUTE,
-  USER_ROUTE,
-  UPDATE_ROUTE,
-  CARD_PATH,
-  ADMIN_ONLY_PATH,
-  UPDATE_PATH,
-  ONBOARDING_PATH,
-  ACTION_ROUTE,
+	SESSION_COOKIE_NAME,
+	LOGIN_ROUTE,
+	SIGNUP_ROUTE,
+	ONBOARDING_ROUTE,
+	CARD_ROUTE,
+	DASHBOARD_ROUTE,
+	USER_ROUTE,
+	UPDATE_ROUTE,
+	CARD_PATH,
+	ADMIN_ONLY_PATH,
+	UPDATE_PATH,
+	ONBOARDING_PATH,
+	ACTION_ROUTE,
 } from "@/constants";
 
 const protectedRoutes = [
-  ONBOARDING_ROUTE,
-  CARD_ROUTE,
-  DASHBOARD_ROUTE,
-  USER_ROUTE,
-  UPDATE_ROUTE,
-  ACTION_ROUTE,
+	ONBOARDING_ROUTE,
+	CARD_ROUTE,
+	DASHBOARD_ROUTE,
+	USER_ROUTE,
+	UPDATE_ROUTE,
+	ACTION_ROUTE,
 ];
 const authRoutes = [LOGIN_ROUTE, SIGNUP_ROUTE];
 
 export default async function middleware(request: NextRequest) {
-  const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
-  const { pathname } = request.nextUrl;
-  if (!session) {
-    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-      const loginURL = new URL(LOGIN_ROUTE, request.url);
-      return NextResponse.redirect(loginURL);
-    }
-    return NextResponse.next();
-  }
-  if (authRoutes.some((route) => pathname === route)) {
-    const onboardingRoute = new URL(ONBOARDING_ROUTE, request.url);
-    return NextResponse.redirect(onboardingRoute);
-  }
-  return NextResponse.next();
+	const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
+	const { pathname } = request.nextUrl;
+	if (!session) {
+		if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+			const loginURL = new URL(LOGIN_ROUTE, request.url);
+			return NextResponse.redirect(loginURL);
+		}
+		return NextResponse.next();
+	}
+	if (authRoutes.some((route) => pathname === route)) {
+		const onboardingRoute = new URL(ONBOARDING_ROUTE, request.url);
+		return NextResponse.redirect(onboardingRoute);
+	}
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    ONBOARDING_PATH,
-    CARD_PATH,
-    ADMIN_ONLY_PATH,
-    UPDATE_PATH,
-  ],
+	matcher: [
+		"/((?!api|_next/static|_next/image|favicon.ico).*)",
+		ONBOARDING_PATH,
+		CARD_PATH,
+		ADMIN_ONLY_PATH,
+		UPDATE_PATH,
+	],
 };
