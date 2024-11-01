b46c35a8f8a456c57e3e9bb30e54a4d10a809a5e
reagan13 <reaganmabawad@gmail.com>
2024-11-02 07:48:54 +0800

    style: remove borders style

--- app/(public)/(authpages)/(password)/layout.tsx
+++ app/(public)/(authpages)/(password)/layout.tsx
@@ -7,8 +7,8 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
 	return (
 		<main className="flex justify-center items-center  min-h-screen p-4">
-			<div className="flex bg-[#21C15C] h-[577px] max-w-[900px] w-full  rounded-md">
-				<div className="hidden md:flex flex-1 items-center rounded-md overflow-hidden border-2 border-blue-500">
+			<div className="flex bg-[#21C15C] h-[577px] max-w-[900px] w-full rounded-md">
+				<div className="hidden md:flex flex-1 items-center rounded-md overflow-hidden">
 					<BackGround />
 				</div>
 				<div className="flex flex-1 items-center justify-center z-10 ">
