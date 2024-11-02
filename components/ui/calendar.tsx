aba5590140b463af2b3827e222ac605b4279490d
Zeff <zeff.somera@volatility.com.au>
2024-11-02 19:56:10 +0800

    fix webpack error

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
@@ -53,8 +51,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-          components={{
-        
+      components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
