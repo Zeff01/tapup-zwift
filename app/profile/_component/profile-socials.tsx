01d92c230f255185d51a41c5b6e745cd57617895
reyleal27 <reyvincentleal@gmail.com>
2024-11-02 14:33:44 +0800

    feat: add socials in profile view

---
+++ app/profile/_component/profile-socials.tsx
@@ -0,0 +1,20 @@
+import { RiFacebookFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+
+const style = {
+    width:'24px',
+    height: '24px',
+  cursor: 'pointer',
+}
+const ProfileSocials = () => {
+  return (
+    <section className="flex gap-3 w-[15rem]  pt-5 pl-5">
+      <RiFacebookFill style={style}/>
+      <FaLinkedinIn style={style}/>
+      <FiInstagram style={style} />
+    </section>
+  );
+};
+
+export default ProfileSocials;
