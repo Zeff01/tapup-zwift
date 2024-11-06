9d004a14abaa4ec9ec97da8ca069ecd9f7314d66
reyleal27 <reyvincentleal@gmail.com>
2024-11-06 21:45:03 +0800

    style: remove text-color

--- components/profile/footer.tsx
+++ components/profile/footer.tsx
@@ -9,16 +9,16 @@ const Footer = ({
   number,
 }: UserProfile) => {
   return (
-    <footer className="flex justify-evenly h-[4rem] max-w-[320px] mx-auto shadow-xl">
+    <footer className="flex justify-evenly h-[4rem] max-w-[320px] mx-auto shadow-xl text-black">
       <div className="flex items-center gap-1 ">
-        <MdOutlineEmail className="text-[#1A1919CC]" />
-        <h2 className="text-[#1A1919CC] text-xs md:text-base">
+        <MdOutlineEmail/>
+        <h2 className=" text-xs md:text-base">
           {firstName ? firstName + " " + lastName : "Anonymous"}
         </h2>
       </div>
       <div className="flex items-center gap-1">
-        <FiPhoneCall className="text-[#1A1919CC]" />
-        <h2 className="text-[#1A1919CC] text-xs md:text-base">{number || "+63123456789"}</h2>
+        <FiPhoneCall />
+        <h2 className="text-xs md:text-base">{number || "+63123456789"}</h2>
       </div>
     </footer>
   );
