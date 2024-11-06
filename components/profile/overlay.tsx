ac7acb29346b51f65aa7ea5e9a1747932a9ba2c2
reyleal27 <reyvincentleal@gmail.com>
2024-11-06 21:50:31 +0800

    feat: add menu overlay

---
+++ components/profile/overlay.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const menuItems = [
+  { label: "Dashboard", href: "/dashboard" },
+  { label: "Profile", href: "/profile" },
+  { label: "My Cards", href: "/cards" },
+];
+
+interface Props {
+  handleMobileMenu: () => void;
+}
+
+const OverlayMenu: React.FC<Props> = ({ handleMobileMenu }) => {
+  const pathname = usePathname();
+
+  return (
+    <div className="">
+      <Sheet>
+        <SheetTrigger asChild>
+          <div
+            className="lg:hidden cursor-pointer mr-4 z-10"
+            onClick={handleMobileMenu}
+          >
+            <RxHamburgerMenu size={20} />
+          </div>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <div className="flex flex-col gap-3 items-center text-xl">
+            {menuItems.map((item, index) => (
+              <Link
+                key={index}
+                href={item.href}
+                className={`${item.href === pathname ? "text-green-600 border-b-2 border-greenTitle" : ""} hover:text-green-500`}
+                onClick={handleMobileMenu}
+              >
+                {item.label}
+              </Link>
+            ))}
+            <Button className="bg-green-500 hover:bg-green-700">
+              Activate
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default OverlayMenu;
