2db7bc806d1542bc77901180768534068dd1e602
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-17 21:29:06 +0800

    feat: complete design-option page

---
+++ app/(secured)/(user)/(boarded)/cards/design-option/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import VirtualCard from "./components/virtualCard";
+
+export default function DesignOption() {
+  return (
+    <div className=" min-h-screen">
+      <VirtualCard />
+    </div>
+  );
+}
