031d43d8014d9149b575d85a22a481a092f71307
Kaleidos <david.estrelloso.tribugenia@gmail.com>
2024-11-26 18:56:55 +0800

    fix card update

--- src/app/(secured)/(user)/(boarded)/user/update/[userId]/_components/update-user-form.tsx
+++ src/app/(secured)/(user)/(boarded)/user/update/[userId]/_components/update-user-form.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import CardsAndUsersFields from "@/components/forms/CardsAndUsersUpdateFields";
+import MultiStepFormUpdate from "@/components/forms/multi-step-form-update";
 import { USER_ROLE_ENUMS } from "@/constants";
 import { useUserContext } from "@/providers/user-provider";
 import { ExtendedUserInterface } from "@/types/types";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const UpdateUserForm = ({ data }: { data: ExtendedUserInterface }) => {
   const { user } = useUserContext();
+  const router = useRouter();
+
+  if (!user) return router.push("/dashboard");
 
   if (user?.role === USER_ROLE_ENUMS.ADMIN && data.id !== user.uid)
-    return <CardsAndUsersFields userData={data} />;
+    return <MultiStepFormUpdate userData={user!} />;
 
-  return <CardsAndUsersFields isCurrentUser userData={user!} />;
+  return <MultiStepFormUpdate isCurrentUser userData={user!} />;
 };
 
 export default UpdateUserForm;
