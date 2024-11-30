856add8558bfad89cbb8f06f579503e6179affd9
sairlseb <cyrilseberiaga@gmail.com>
2024-11-30 15:17:43 +0800

    added social buttons, cta buttons and partial company details

--- components/templates/Template7.tsx
+++ components/templates/Template7.tsx
@@ -2,16 +2,16 @@ import { Card } from "@/types/types";
 import Image from "next/image";
 import { CiMail, CiPhone, CiSaveDown2 } from "react-icons/ci";
 import {
-  FaXTwitter,
   FaFacebook,
-  FaYoutube,
   FaInstagram,
   FaLinkedin,
-  FaWhatsapp,
-  FaSkype,
   FaGlobe,
+  FaEnvelope,  
+  FaPhone,
+  FaFacebookMessenger
 } from "react-icons/fa6";
 import { downloadVCard } from "@/lib/utils";
+import {Button} from "@/components/ui/button";
 
 const Template7 = ({
     profilePictureUrl,
@@ -85,25 +85,51 @@ const Template7 = ({
                 </div>
                 {/* PERSONAL INFORMATION */}
                 <div className="mt-28 space-y-1 pl-5">
-                        {firstName ? (
-                            <h1 className="text-xl font-bold mt-4 text-blue-600 ">
-                            {firstName + " " + lastName}
-                            </h1>
-                        ) : (
-                            <h1 className="text-xl font-bold mt-2 text-blue-600 ">Hussain Watkins</h1>
-                        )}
-
-                        <p className="font-semibold text-gray-900 text-xs">
-                            {position ?? "Chief Technology Officer"}
-                        </p>
-
-                        <p className=" text-gray-500 text-xs">
-                            {email ?? "H.Watkins@gmail.com"}
-                        </p>
-
-                        <p className=" text-gray-500 text-xs"> {number ?? +639123456789}</p>
+                    {firstName ? (
+                        <h1 className="text-xl font-bold mt-4 text-blue-600 ">
+                        {firstName + " " + lastName}
+                        </h1>
+                    ) : (
+                        <h1 className="text-xl font-bold mt-2 text-blue-600 ">Hussain Watkins</h1>
+                    )}
+                    <p className="font-semibold text-gray-500">
+                        {position ?? "Chief Technology Officer"}
+                    </p>
+                    <p className=" text-gray-500 text-xs">
+                        {email ?? "H.Watkins@gmail.com"}
+                    </p>
+                    <p className=" text-gray-500 text-xs"> {number ?? +639123456789}</p>
+                </div>
+                {/* SOCIALS */}
+                <div className="flex pl-5 mt-3 gap-2">
+                    <Button className="rounded-full"><FaFacebook /></Button>
+                    <Button className="rounded-full"><FaFacebookMessenger /></Button>
+                    <Button className="rounded-full"><FaInstagram /></Button>
+                    <Button className="rounded-full"><FaLinkedin /></Button>
+                    <Button className="rounded-full"><FaEnvelope /></Button>
+                    <Button className="rounded-full"><FaPhone /></Button>
+                    <Button className="rounded-full"><FaGlobe /></Button>
+                </div>
+                {/* CTA BUTTONS */}
+                <div className="grid grid-cols-3 gap-4 mt-5">
+                    <Button className="col-span-2 bg-blue-600 text-white">Email Me</Button>
+                    <Button className="bg-gray-500 text-white">Save</Button>
+                </div>
+                {/*COMPANY DETAILS */}
+                <div className="flex flex-col mt-5">
+                    <div className="text-xl text-blue-600 font-bold mb-5">
+                        {company ?? 'ABC Company'}
                     </div>
+                    <div className="flex flex-col gap-y-2">
+                        <div className="text-gray-500 text-xs font-semibold">Company Overview</div>
+                        <div className="text-gray-500 text-[10px] font-normal leading-4">
+                        Lorem ipsum dolor sit amet consectetur. Commodo non imperdiet tempus orci non id nibh 
+                        faucibus. Laoreet at hendrerit at viverra dignissim consequat posuere mi cras.
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
