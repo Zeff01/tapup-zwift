7f3a3bba4408cbddcbf3be6fbf732386e20df552
reagan13 <reaganmabawad@gmail.com>
2024-12-05 08:11:10 +0800

    fix: adjust layout height and update camera icon styling

--- src/app/(secured)/(user)/(boarded)/retrieve/page.tsx
+++ src/app/(secured)/(user)/(boarded)/retrieve/page.tsx
@@ -28,7 +28,7 @@ const Page: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center p-6 h-screen relative">
+    <div className="flex flex-col items-center justify-center p-6 h-full relative">
       {/* Title */}
       <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-green-500 dark:text-green-400">
         Scan your card
@@ -73,7 +73,7 @@ const Page: React.FC = () => {
         onClick={handleCameraClick}
         className="focus:outline-none absolute bottom-12 sm:bottom-10 md:bottom-8"
       >
-        <Camera className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 text-green-500 dark:text-green-400" />
+        <Camera className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 text-green-500" />
       </button>
     </div>
   );
