9677ecf2487e97fa4093906223904b1402c0e0e7
reyleal27 <reyvincentleal@gmail.com>
2024-12-18 03:54:47 +0800

    feat: add payment route

--- src/app/(secured)/(user)/(boarded)/dashboard/page.tsx
+++ src/app/(secured)/(user)/(boarded)/dashboard/page.tsx
@@ -4,8 +4,9 @@ import Dashboard from "./_components/dashboard";
 export default function DashboardPage() {
   return (
     <div className="flex-1">
-      {/* <Dashboard /> */}
-      <ComingSoon />
+      <Dashboard />
+      {/* <ComingSoon /> */}
+     
     </div>
   );
 }
