dfc234687353374575801f714f2e0b5f17f044b6
jmiras <jm@weareindeed.com>
2025-01-07 11:34:44 +0800

    Update LoadingLogo.tsx and firebase.ts

--- components/LoadingLogo.tsx
+++ components/LoadingLogo.tsx
@@ -8,16 +8,20 @@ import GridLoader from "react-spinners/GridLoader";
 export default function LoadingLogo() {
   return (
     <div className="bg-background flex flex-col items-center justify-center h-[100dvh]">
-      <div className="flex flex-col items-center">
-        <GridLoader size={20} color="#6150EB" />
+      <div className="flex flex-col items-center gap-12 ">
         <Image
-          src="/assets/zwift-logo.png"
+          src="/assets/tap-up-logo-white.png"
           alt="Company Logo"
           width={150}
           height={150}
           priority
-          className="mx-auto mt-4"
+          className="mx-auto mt-4 dark:block"
         />
+        <div className='flex space-x-2 justify-center items-center gap-3 dark:invert'>
+          <div className='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+          <div className='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+          <div className='h-5 w-5 bg-black rounded-full animate-bounce'></div>
+        </div>
       </div>
     </div>
   );
--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: "52ae93aa91c7979ce3c0dca48e5b33676bd4324ad56abfe3ef5f420bfbdbf054dc3639847ad3287c40660fc999f0f771348d2b66f2b183ada5971e9ab246b1ad18ce0fb383ec58b3b3d027d1993a17bb3b34060ffc254149d7299b19f0bfb2557d68a31ce886ed3a0667d9678607a6e29a70462ca3d25ecf4040aacc515fe3ae77e0d09a1ffe3b7f832af40b00cc6f43ad94e09ff78cfa0f060c36c54bf41246eae427463daa8e757172f29ecd079bed546ab1fc785ccaaf18661e1c016b038edcc398c964e0a1965afae2f272e7640d62c3d802ce797cdf308f375e8b311f110037e4d4584fdd696f746932cd7c8fe73f7034928d83454de251621273b02964",
+  authDomain: "tapup-zwift.firebaseapp.com",
+  projectId: "tapup-zwift",
+  storageBucket: "tapup-zwift.firebasestorage.app",
+  messagingSenderId: "884529098129",
+  appId: "1:884529098129:web:c087abc7b8618109dffe30",
+  measurementId: "G-SZR6YZR9HD"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
