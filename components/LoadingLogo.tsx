bb36ba09508d0923df70c17e5ad38d500c83d2dc
jmiras <jm@weareindeed.com>
2025-01-14 12:01:24 +0800

    done adding screen icon based on user theme, for review

--- components/LoadingLogo.tsx
+++ components/LoadingLogo.tsx
@@ -1,27 +1,36 @@
 "use client";
+import { useTheme } from "next-themes";
 import Image from "next/image";
-import dynamic from "next/dynamic";
-import GridLoader from "react-spinners/GridLoader";
+import { useEffect, useState } from "react";
 
-// const GridLoader = dynamic(() => import("react-spinners/GridLoader"));
+export default function LogoComponent() {
+  const { theme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export default function LoadingLogo() {
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? resolvedTheme : theme;
+  console.log("Current Theme:", currentTheme);
   return (
-    <div className="bg-background flex flex-col items-center justify-center h-[100dvh]">
-      <div className="flex flex-col items-center gap-12 ">
-        <Image
-          src="/assets/tap-up-header-logo.png"
-          alt="Company Logo"
-          width={150}
-          height={150}
-          priority
-          className="mx-auto mt-4 dark:hidden"
-        />
-        <div className='flex space-x-2 justify-center items-center gap-3 dark:invert'>
-          <div className='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-          <div className='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-          <div className='h-5 w-5 bg-black rounded-full animate-bounce'></div>
-        </div>
+    <div className="flex flex-col items-center justify-center gap-12 min-h-screen">
+      <Image
+        src={
+          currentTheme === "dark"
+            ? "/assets/tap-up-logo-white.png"
+            : "/assets/tap-up-header-logo.png"
+        }
+        alt="Company Logo"
+        width={150}
+        height={150}
+        priority
+        className="mx-auto mt-4"
+        style={{ objectFit: "cover" }}
+      />
+      <div className='flex space-x-2 justify-center items-center gap-3 dark:invert'>
+        <div className="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-5 w-5 bg-black rounded-full animate-bounce"></div>
       </div>
     </div>
   );
