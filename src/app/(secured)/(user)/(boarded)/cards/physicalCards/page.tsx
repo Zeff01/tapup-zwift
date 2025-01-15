a72c7397c313dc41d25cf8ba328389496ae235e8
reagan13 <reaganmabawad@gmail.com>
2025-01-15 10:57:01 +0800

    feat: add new page component for OrderPhysicalCard

---
+++ src/app/(secured)/(user)/(boarded)/cards/physicalCards/page.tsx
@@ -0,0 +1,11 @@
+import OrderPhysicalCard from "./components/OrderPhysicalCard";
+
+const Page = () => {
+  return (
+    <main className="w-full flex justify-center h-full">
+      <OrderPhysicalCard />
+    </main>
+  );
+};
+
+export default Page;
