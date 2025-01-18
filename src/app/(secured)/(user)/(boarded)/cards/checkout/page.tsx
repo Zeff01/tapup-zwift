b4223dff45c81bfed57997ac15d69f071fb36dfe
reagan13 <reaganmabawad@gmail.com>
2025-01-19 05:56:36 +0800

    feat: add Checkout page with CardShop component

---
+++ src/app/(secured)/(user)/(boarded)/cards/checkout/page.tsx
@@ -0,0 +1,11 @@
+import CardShop from "../_components/CardShop";
+
+const Page = () => {
+  return (
+    <main className="w-full flex justify-center h-full">
+      <CardShop />
+    </main>
+  );
+};
+
+export default Page;
