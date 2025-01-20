e8e0f0e2c7ab59813a3297cb075ffe7f8e274a6a
reagan13 <reaganmabawad@gmail.com>
2025-01-20 14:45:18 +0800

    feat: create DeliveryOption page to render DeliveryOption component

---
+++ src/app/(secured)/(user)/(boarded)/cards/deliveryOption/page.tsx
@@ -0,0 +1,11 @@
+import DeliveryOption from "../_components/DeliveryOption";
+
+const Page = () => {
+  return (
+    <main className="w-full flex justify-center h-full">
+      <DeliveryOption />
+    </main>
+  );
+};
+
+export default Page;
