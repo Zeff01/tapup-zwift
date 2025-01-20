ecf843fdb790ebca3392dad9b0e7d7da8d0e5eea
reagan13 <reaganmabawad@gmail.com>
2025-01-20 10:54:07 +0800

    feat: create ShippingAddress page

---
+++ src/app/(secured)/(user)/(boarded)/cards/shippingAddress/page.tsx
@@ -0,0 +1,11 @@
+import ShippingAddress from "../_components/ShippingAddress";
+
+const Page = () => {
+  return (
+    <main className="w-full flex justify-center h-full">
+      <ShippingAddress />
+    </main>
+  );
+};
+
+export default Page;
