fb830a65f2eabb012632f73e4aaf040f5c819921
reagan13 <reaganmabawad@gmail.com>
2025-01-22 13:34:36 +0800

    feat: integrate delivery option selection with shipping info provider

--- src/app/(secured)/(user)/(boarded)/cards/_components/DeliveryOption.tsx
+++ src/app/(secured)/(user)/(boarded)/cards/_components/DeliveryOption.tsx
@@ -4,19 +4,44 @@ import React, { useEffect, useState } from "react";
 import NavBar from "./Navbar";
 import Courier from "@/components/Courier";
 import { courierList } from "@/constants";
+import { useShippingInfo } from "@/providers/shipping-info-provider";
+
 const DeliveryOption = () => {
   const [selectedOption, setSelectedOption] = useState<number | null>(null);
+  const { state, setState } = useShippingInfo();
 
   const handleSelect = (index: number) => {
     setSelectedOption(index);
+    setState((prevState) => ({
+      ...prevState,
+      deliveryOption: courierList[index],
+    }));
   };
 
   useEffect(() => {
-    // Set the first courier as the selected option when the component mounts
-    if (courierList.length > 0) {
+    // Set the selected option based on the delivery option in the state
+    const currentDeliveryOption = state.deliveryOption;
+    if (currentDeliveryOption) {
+      const index = courierList.findIndex(
+        (courier) => courier.name === currentDeliveryOption.name
+      );
+      if (index !== -1) {
+        setSelectedOption(index);
+      } else {
+        setSelectedOption(0);
+        setState((prevState) => ({
+          ...prevState,
+          deliveryOption: courierList[0],
+        }));
+      }
+    } else if (courierList.length > 0) {
       setSelectedOption(0);
+      setState((prevState) => ({
+        ...prevState,
+        deliveryOption: courierList[0],
+      }));
     }
-  }, []);
+  }, [setState, state.deliveryOption]);
 
   return (
     <div className="relative max-h-screen flex flex-col max-w-sm w-full ">
@@ -26,7 +51,7 @@ const DeliveryOption = () => {
       <div className="space-y-3 mx-4">
         {courierList.map((courier, index) => (
           <Courier
-            key={courier.id}
+            key={index}
             name={courier.name}
             image={courier.image}
             shippingFee={courier.shippingFee}
