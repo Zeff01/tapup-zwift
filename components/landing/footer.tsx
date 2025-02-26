f1be55f85c0bb51a0b7dbf2e995244ae76242f9d
reyleal27 <reyvincentleal@gmail.com>
2025-02-26 22:19:53 +0800

    feat: add links for contact us, policy,term & conditions

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import Newsletter from "./newsletter";
-import logo from "@/public/assets/tap-up-logo-white.png";
-import Image from "next/image";
 import TapupLogo from "../svgs/TapupLogo";
 
 const list = ["Terms", "Privacy", "Cookies"];
@@ -14,24 +12,50 @@ const socials = [
   { title: "linkedin", image: "/assets/Icon-linkedin.png" },
   { title: "Discord", image: "/assets/icon-discord.png" },
 ];
+
+const listItem = {
+  list: ["About","Contact","Privacy Policy", "Terms and Conditions"],
+  url: ["/#about","/contact","/privacy-policy", "/terms-conditions"],
+};
+
 const Footer = () => {
   return (
     <footer className="pb-8 pt-16" id="contact">
-      <Newsletter />
+      {/* <Newsletter /> */}
       <div className="flex mt-8 flex-col sm:flex-row w-full gap-4 justify-around pt-6 items-center">
         <div className="mx-auto aspect-[3.63] h-9">
           <Link href="/">
             <TapupLogo className="w-full h-full" />
           </Link>
         </div>
-        <div className="flex gap-5 mx-auto">
-          {list.map((item, index) => (
-            <Link href="/" key={index} className="font-semibold">
+
+        {/* remove list and socials temporarily */}
+        <div className="gap-8 mx-auto flex">
+         
+          <div>
+          <h2 className="font-semibold text-lg">Information</h2>
+          <div className="flex flex-col gap-1">
+           <Link href="/testimonials" className="hover:text-hoverColor">Testimonials</Link>
+          </div>
+          </div>
+
+            <div className="">
+          <h2 className="font-semibold text-lg">Company</h2>
+          <div className="flex flex-col gap-1">
+           {listItem.list.map((item, index) => (
+             <Link
+             href={listItem.url[index]}
+             key={index}
+             className="hover:text-hoverColor"
+             >
               {item}
             </Link>
-          ))}
+          ))} 
+          </div>
         </div>
-        <ul className="flex gap-4 mx-auto">
+        </div>
+        
+        {/* <ul className="flex gap-4 mx-auto">
           {socials.map((item, index) => (
             <li key={index} className="relative w-6 h-6">
               <Link href="/">
@@ -44,7 +68,7 @@ const Footer = () => {
               </Link>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </footer>
   );
