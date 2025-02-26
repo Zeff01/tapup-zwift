1d1db5c387a5baa58b2f714fe921c261ed15d023
reyleal27 <reyvincentleal@gmail.com>
2025-02-26 14:51:48 +0800

    refactor: redirect to orders/pending

--- src/app/(secured)/(user)/(boarded)/orders/page.tsx
+++ src/app/(secured)/(user)/(boarded)/orders/page.tsx
@@ -1,16 +1,28 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
+import LoadingLogo from "@/components/LoadingLogo";
 
 export default function OrdersHomePage() {
   const router = useRouter();
   const pathname = usePathname();
+  const [loading, setLoading] = useState(true);
 
+ 
   useEffect(() => {
     if (pathname === "/orders") {
+      setLoading(true);
       router.push("/orders/pending");
+    } else {
+      setLoading(false);
     }
   }, [pathname, router]);
+
+  if (loading) {
+    return (
+    <LoadingLogo/>
+    );
+  }
 
   return null;
 }
