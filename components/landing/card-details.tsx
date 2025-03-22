33ca3de6092d59d7fcd52f4c3c02d64b75bb00b7
reagan13 <reaganmabawad@gmail.com>
2025-03-22 16:19:17 +0800

    feat: add price display to card details and update footer layout

--- components/landing/card-details.tsx
+++ components/landing/card-details.tsx
@@ -13,17 +13,19 @@ const CardDetails: React.FC<CardDetailsProps> = ({ card }) => {
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-[3.5vw] leading-tight md:leading-none text-center font-black">
         {card?.title}
       </h2>
-
+      <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-[3vw] leading-tight md:leading-none text-center font-bold text-greenText my-3">
+        ₱ {card?.price}
+      </h2>
       <pre className="text-lg w-full max-w-lg leading-snug md:leading-8 sm:text-xl text-muted-foreground text-justify mt-6 whitespace-pre-line font-sans mx-auto h-56 p-4 rounded-md ">
         {card?.description}
       </pre>
 
       <Link href="/signup">
-      <Button className="uppercase max-w-[15rem] px-8 py-6 w-full text-white bg-buttonColor mt-[27px] font-bold flex items-center mx-auto hover:bg-hoverColor">
-        Sign up & Activate
-        <LogIn />
+        <Button className="uppercase max-w-[15rem] px-8 py-6 w-full text-white bg-buttonColor mt-[27px] font-bold flex items-center mx-auto hover:bg-hoverColor">
+          Sign up & Activate
+          <LogIn />
         </Button>
-        </Link>
+      </Link>
     </section>
   );
 };
--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -14,8 +14,8 @@ const socials = [
 ];
 
 const listItem = {
-  list: ["About","Contact","Privacy Policy", "Terms and Conditions"],
-  url: ["/#about","/contact","/privacy-policy", "/terms-conditions"],
+  list: ["About", "Contact", "Privacy Policy", "Terms and Conditions"],
+  url: ["/#about", "/contact", "/privacy-policy", "/terms-conditions"],
 };
 
 const Footer = () => {
@@ -23,39 +23,50 @@ const Footer = () => {
     <footer className="pb-8 pt-16" id="contact">
       {/* hide newsletter */}
       {/* <Newsletter /> */}
-      
+
       <div className="flex mt-8 flex-col sm:flex-row w-full gap-4 justify-around pt-6 items-center">
-        <div className="mx-auto aspect-[3.63] h-9">
-          <Link href="/">
-            <TapupLogo className="w-full h-full" />
+        <div className="mx-auto text-center">
+          <div className="aspect-[3.63] h-9 mx-auto">
+            <Link href="/">
+              <TapupLogo className="w-full h-full" />
+            </Link>
+          </div>
+          <Link
+            href="mailto:codebility.dev@gmail.com"
+            className="block hover:text-hoverColor mt-2"
+          >
+            codebility.dev@gmail.com
+          </Link>
+          <Link href="tel:+63921090799" className="block hover:text-hoverColor">
+            +63921090799
           </Link>
         </div>
 
-     
         <div className="gap-8 mx-auto flex">
           <div>
-          <h2 className="font-semibold text-lg">Information</h2>
-          <div className="flex flex-col gap-1">
-           <Link href="/testimonials" className="hover:text-hoverColor">Testimonials</Link>
+            <h2 className="font-semibold text-lg">Information</h2>
+            <div className="flex flex-col gap-1">
+              <Link href="/testimonials" className="hover:text-hoverColor">
+                Testimonials
+              </Link>
+            </div>
           </div>
-          </div>
-            
-          <div>
-          <h2 className="font-semibold text-lg">Company</h2>
-          <div className="flex flex-col gap-1">
-           {listItem.list.map((item, index) => (
-             <Link
-             href={listItem.url[index]}
-             key={index}
-             className="hover:text-hoverColor"
-             >
-              {item}
-            </Link>
-          ))} 
-          </div>
-        </div>
-        </div>
 
+          <div>
+            <h2 className="font-semibold text-lg">Company</h2>
+            <div className="flex flex-col gap-1">
+              {listItem.list.map((item, index) => (
+                <Link
+                  href={listItem.url[index]}
+                  key={index}
+                  className="hover:text-hoverColor"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
 
         {/* hide socials */}
         {/* <ul className="flex gap-4 mx-auto">
