a51bdeab71d4252361ada6fa40a81ffe06e4aebf
David <david.estrelloso.tribugenia@gmail.com>
2025-05-28 10:48:02 +0800

    fix menu

--- components/boarded/topbar.tsx
+++ components/boarded/topbar.tsx
@@ -5,7 +5,9 @@ import OverlayMenu from "./NavigationInside";
 const TopbarBoarded = () => {
   return (
     <div className="flex lg:hidden py-4 border-b px-4 sticky top-0 z-50 bg-transparent backdrop-blur-xl items-center ">
-      <TapupLogo className="scale-[.65] -ml-4" />
+      <div className="w-20 lg:w-28 aspect-[130/48]">
+        <TapupLogo />
+      </div>
       <span className="absolute -translate-y-1/2 top-1/2 right-4">
         <OverlayMenu />
       </span>
