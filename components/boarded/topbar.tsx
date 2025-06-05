15332f3f24dba48e8666cd23cca45c2439f430c2
Wendel Sabayo <wendelsabayo999@gmail.com>
2025-06-05 10:09:47 +0800

    TapUp logo linked to landing

--- components/boarded/topbar.tsx
+++ components/boarded/topbar.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import TapupLogo from "../svgs/TapupLogo";
 import OverlayMenu from "./NavigationInside";
 import Cart from "../cart/Cart";
+import Link from "next/link";
 
 const TopbarBoarded = () => {
   return (
     <div className="flex justify-between lg:hidden py-4 border-b px-4 sticky top-0 z-50 bg-transparent backdrop-blur-xl items-center ">
-      <div className="w-20 lg:w-28 aspect-[130/48]">
-        <TapupLogo />
-      </div>
+      <Link href={"/"}>
+        {" "}
+        <div className="w-20 lg:w-28 aspect-[130/48]">
+          <TapupLogo />
+        </div>{" "}
+      </Link>
       <div className="flex gap-4 items-center">
         <div className="pr-2">
           <Cart />
