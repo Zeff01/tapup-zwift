b359a7cb4bc2971b22055d2f4dae1cc43b135312
JonashGana <jonashtiglao@gmail.com>
2025-06-23 23:26:21 +0800

    refactor recurring webhook

--- src/app/api/webhooks/xendit/recurring-webhook/route.ts
+++ src/app/api/webhooks/xendit/recurring-webhook/route.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { addSubscription } from "@/lib/firebase/actions/user.action";
+import {
+  addSubscription,
+  addCard,
+  createTransaction,
+} from "@/lib/firebase/actions/user.action";
+import { TransactionType } from "@/types/types";
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,26 +14,33 @@ export async function POST(req: NextRequest) {
       event.event === "recurring.plan.activated" ||
       event.event === "recurring.cycle.succeeded"
     ) {
-      const cardIds = event.data.metadata?.cardIds;
-      const userId = event.data.metadata?.userId;
-      const cardId = event.data.metadata?.cardId;
+      const {
+        userId,
+        cardItems,
+        customerEmail,
+        customerName,
+        customerPhone,
+        customerAddress,
+        totalAmount,
+      } = event.data.metadata;
       const interval = event.data.schedule.interval;
       const intervalCount = event.data.schedule.interval_count;
+      const customerId = event.data.customer_id;
 
       console.log({
-        cardIds,
-        cardId,
+        userId,
+        cardItems,
+        customerEmail,
+        customerName,
+        customerPhone,
+        customerAddress,
+        totalAmount,
         interval,
         intervalCount,
-        userId,
+        customerId,
       });
 
-      if (
-        ((!cardIds || cardIds.length === 0) &&
-          (!cardId || cardId.length === 0)) ||
-        !interval ||
-        !intervalCount
-      ) {
+      if (!cardItems || cardItems.length === 0 || !interval || !intervalCount) {
         return NextResponse.json(
           { error: "Missing required subscription data" },
           { status: 400 }
@@ -43,15 +55,83 @@ export async function POST(req: NextRequest) {
 
       const subscriptionDays = intervalMapping[interval] * intervalCount;
 
-      if (cardIds) {
-        await addSubscription({
-          cardIds: [...(cardIds || []), ...(cardId || [])],
-          subscriptionDays,
-          ...(userId && { userId: userId }),
-        });
+      const addCardPromises = cardItems.map(
+        (item: { id: string; name: string }) => {
+          return addCard({ id: item.id, name: item.name });
+        }
+      );
 
-        return NextResponse.json({ success: true }, { status: 200 });
-      }
+      const cardIds: string[] = await Promise.all(addCardPromises);
+
+      const transactionData: TransactionType = {
+        amount: totalAmount,
+        cards: cardIds.map((cardIds, i) => ({
+          id: cardIds,
+          name: cardItems[i].name,
+        })),
+        receiver: {
+          customerId: customerId,
+          customerName: customerName,
+          customerEmail: customerEmail,
+          customerPhone: customerPhone,
+          customerAddress: customerAddress,
+        },
+        status: "pending",
+      };
+
+      await createTransaction(transactionData);
+
+      await addSubscription({
+        cardIds: cardIds,
+        subscriptionDays,
+        ...(userId && { userId: userId }),
+      });
+
+      return NextResponse.json({ success: true }, { status: 200 });
+
+      // const cardIds = event.data.metadata?.cardIds;
+      // const userId = event.data.metadata?.userId;
+      // const cardId = event.data.metadata?.cardId;
+      // const interval = event.data.schedule.interval;
+      // const intervalCount = event.data.schedule.interval_count;
+
+      // console.log({
+      //   cardIds,
+      //   cardId,
+      //   interval,
+      //   intervalCount,
+      //   userId,
+      // });
+
+      // if (
+      //   ((!cardIds || cardIds.length === 0) &&
+      //     (!cardId || cardId.length === 0)) ||
+      //   !interval ||
+      //   !intervalCount
+      // ) {
+      //   return NextResponse.json(
+      //     { error: "Missing required subscription data" },
+      //     { status: 400 }
+      //   );
+      // }
+
+      // const intervalMapping: Record<string, number> = {
+      //   DAY: 1,
+      //   MONTH: 30,
+      //   YEAR: 365,
+      // };
+
+      // const subscriptionDays = intervalMapping[interval] * intervalCount;
+
+      // if (cardIds) {
+      //   await addSubscription({
+      //     cardIds: [...(cardIds || []), ...(cardId || [])],
+      //     subscriptionDays,
+      //     ...(userId && { userId: userId }),
+      //   });
+
+      //   return NextResponse.json({ success: true }, { status: 200 });
+      // }
     }
 
     return NextResponse.json(
