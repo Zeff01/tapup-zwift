7cb8cc8d430145b8137851d55160703d8cb05988
JonashGana <jonashtiglao@gmail.com>
2025-06-25 20:32:23 +0800

    created action to clear user cart

--- lib/firebase/actions/cart.action.ts
+++ lib/firebase/actions/cart.action.ts
@@ -7,6 +7,7 @@ import {
   doc,
   getDocs,
   setDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { firebaseDb } from "../firebase";
 import { CartItem } from "@/providers/cart-provider-v2";
@@ -36,6 +37,26 @@ export const getCartByUserUid = async (
   } catch (error) {
     console.error(error);
     throw error;
+  }
+};
+
+export const clearCartByUserId = async (userUid: string) => {
+  try {
+    if (!userUid) throw new Error("User id is not found");
+
+    const cartCollection = collection(firebaseDb, "cart");
+    const q = query(cartCollection, where("userUid", "==", userUid));
+
+    const cartSnap = await getDocs(q);
+    if (cartSnap.empty) {
+      throw new Error("No cart items found for this user");
+    }
+
+    const cartDoc = cartSnap.docs[0];
+    await updateDoc(cartDoc.ref, { cartItems: [] });
+  } catch (error) {
+    console.error("Error clearing cart:", error);
+    throw new Error("Failed to clear cart");
   }
 };
 
