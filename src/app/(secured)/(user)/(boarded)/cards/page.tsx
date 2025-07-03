645a09e93698d858f82840c9b7ca9aefc1805cc4
UzziKyle <uzzielkyleynciong0824@gmail.com>
2025-07-04 07:42:19 +0800

    refactor: remove client wrapper for card page

--- src/app/(secured)/(user)/(boarded)/cards/page.tsx
+++ src/app/(secured)/(user)/(boarded)/cards/page.tsx
@@ -1,7 +1,7 @@
 // app/cards/page.tsx
 import { authCurrentUserv2 } from "@/lib/firebase/auth";
 import { notFound, redirect } from "next/navigation";
-import ClientCardWrapper from "./_components/client-card-wrapper";
+import Cards from "./_components/cards";
 
 export default async function CardPage() {
   const auth = await authCurrentUserv2();
@@ -9,5 +9,9 @@ export default async function CardPage() {
   if (!auth) redirect("/login");
   if (!auth?.role) return notFound();
 
-  return <ClientCardWrapper />;
+  return (
+    <div className={`flex-1`}>
+      <Cards />
+    </div>
+  );
 }
