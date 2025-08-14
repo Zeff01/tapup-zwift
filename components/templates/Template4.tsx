45d9db2f9a0bed48d191afb100e5d90861869109
Wendel Sabayo <wendelsabayo999@gmail.com>
2025-08-14 16:44:06 +0800

    render multi companies in template 2, 3, 4

--- components/templates/Template4.tsx
+++ components/templates/Template4.tsx
@@ -44,6 +44,7 @@ const Template4 = ({
   websiteUrl,
   viberUrl,
   customUrl,
+  companies,
 }: Card) => {
   const userProfile = {
     id,
@@ -63,38 +64,20 @@ const Template4 = ({
         {/* COVERPHOTO AND PROFILE PIC */}
         <div className=" flex flex-col relative w-full  ">
           <div className="w-full  h-48 overflow-hidden">
-            {coverPhotoUrl ? (
+            {coverPhotoUrl && (
               <Image
                 src={coverPhotoUrl}
                 alt="Cover Image"
                 fill
                 className="mx-auto w-full h-48 object-cover   overflow-hidden"
               />
-            ) : (
-              <Image
-                src={"/assets/template1coverphoto.png"}
-                alt="Cover Image"
-                width={400}
-                height={200}
-                className="mx-auto"
-              />
             )}
           </div>
           <div className="absolute -bottom-14 left-4 ">
-            {profilePictureUrl ? (
+            {profilePictureUrl && (
               <div className=" rounded-full border-2 border-white mx-auto overflow-hidden">
                 <Image
                   src={profilePictureUrl}
-                  alt="Profile Image"
-                  width={500}
-                  height={500}
-                  className="rounded-full w-32 h-32"
-                />
-              </div>
-            ) : (
-              <div className=" rounded-full mx-auto flex items-center justify-center">
-                <Image
-                  src={"/assets/template4samplepic.png"}
                   alt="Profile Image"
                   width={500}
                   height={500}
@@ -135,57 +118,68 @@ const Template4 = ({
               {firstName + " " + lastName}
             </h1>
           ) : (
-            <h1 className="text-2xl font-bold mt-2 ">Hussain Watkins</h1>
+            ""
           )}
-          <p className="font-semibold text-gray-600 text-xl">
-            {position ?? "Chief Technology Officer"}
-          </p>
-          <p className=" text-gray-600 text-md">
-            {email ?? "H.Watkins@gmail.com"}
-          </p>
-          <p className=" text-gray-600 text-md"> {number ?? +639123456789}</p>
+          <p className=" text-gray-600 text-md">{email}</p>
+          <p className=" text-gray-600 text-md"> {number}</p>
         </div>
 
         <hr />
 
         {/* COMPANY INFORMATION */}
-        <h2 className="text-4xl font-extrabold mx-auto w-full mt-6 px-4">
-          {company ?? "COMPANY"}
-        </h2>
-        <div className="mt-6 px-4">
-          <h2 className="text-md font-bold">Company Background</h2>
-          <p className="text-xs text-gray-600 mt-4">
-            {companyBackground ??
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-          </p>
+        {companies?.map((c, i) => (
+          <div key={i} className="mb-10 mt-2 px-2">
+            {/* Company Name */}
+            <div className="flex flex-col items-center text-center space-y-1">
+              <h2 className="text-2xl font-bold text-gray-800">{c.company}</h2>
+              <h3 className="text-sm font-medium text-gray-600">
+                {c.position}
+              </h3>
+            </div>
 
-          {/* SERVICE INFORMATION */}
-          <h3 className="text-md font-bold mt-6">Our Services</h3>
-          <p className="text-xs text-gray-600 mt-4">
-            {serviceDescription ??
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-          </p>
-        </div>
+            {/* Background & Services */}
+            <div className="mt-6">
+              {c.companyBackground?.trim() && (
+                <>
+                  <h2 className="text-md font-bold">Company Background</h2>
+                  <p className="text-xs mt-4 text-gray-600">
+                    {c.companyBackground}
+                  </p>
+                </>
+              )}
 
-        <div className="flex flex-col gap-4 mt-6 px-4">
-          {servicePhotos
-            ? servicePhotos.map((photo, index) => (
-                <div key={index} className="col-span-1">
-                  <Image
-                    src={photo}
-                    alt={`Service Photo ${index + 1}`}
-                    width={500}
-                    height={500}
-                    layout="responsive"
-                    className="rounded-md object-cover w-full"
-                  />
-                </div>
-              ))
-            : ""}
-        </div>
+              {c.serviceDescription?.trim() && (
+                <>
+                  <h3 className="text-md font-bold mt-6">Our Services</h3>
+                  <p className="text-xs mt-4 text-gray-600">
+                    {c.serviceDescription}
+                  </p>
+                </>
+              )}
+            </div>
+
+            {/* Service Photos */}
+            {Array.isArray(c.servicePhotos) && c.servicePhotos.length > 0 && (
+              <div className="grid grid-cols-2 gap-4 mt-6">
+                {c.servicePhotos.map((photo, index) => (
+                  <div key={index} className="col-span-1">
+                    <Image
+                      src={photo}
+                      alt={`Service Photo ${index + 1}`}
+                      width={300}
+                      height={300}
+                      layout="responsive"
+                      className="rounded-md object-cover w-full shadow shadow-white overflow-hidden"
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
       {/* SOCIAL MEDIA ICONS */}
-      <div className="flex flex-col justify-center items-center mt-8 mb-4">
+      <div className="flex flex-col justify-center items-center mt-3 mb-4">
         <h1 className="font-bold mb-2">Socials</h1>
         <div className="flex justify-center gap-4 mb-2">
           {facebookUrl && (
@@ -249,10 +243,6 @@ const Template4 = ({
         </div>
       </div>
       {/* FOOTER */}
-      <h2 className="text-xs font-extrabold mx-auto w-full text-center mb-2">
-        {company ?? "COMPANY"}
-      </h2>
-
       <div className="flex flex-col mt-3 mb-1 items-center gap-1 text-center text-xs">
         <a
           href={userProfile?.customUrl ?? userProfile?.websiteUrl ?? "#"}
